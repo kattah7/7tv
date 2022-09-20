604c498bc97d6f9ceca4c1fd6913542952815bb4
kattah7 <70865282+kattah7@users.noreply.github.com>
2022-09-21 03:16:53 +0900

    New design (#2)
    * Added style css
    
    * Better design for global page
    
    * added search
    
    * added login
    
    * changed port

--- src/rpc/bot.ts
+++ src/rpc/bot.ts
@@ -1,6 +1,8 @@
 import fetch from "node-fetch";
 
-const HOSTNAME = "http://localhost:5000"
+
+const HOSTNAME = "http://localhost:5007"
+
 
 exports.joinChannelByUsername = async (username) => {
     const r = await fetch(`${HOSTNAME}/bot/join?username=${encodeURIComponent(username)}`, {
