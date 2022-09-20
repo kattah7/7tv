ab04012768e5236d88d6ac6fb93c64fff48e2472
Kattah <kattahpro@gmail.com>
2022-09-21 03:15:26 +0900

    changed port

--- src/rpc/bot.ts
+++ src/rpc/bot.ts
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 
-const HOSTNAME = "http://localhost:5000"
+const HOSTNAME = "http://localhost:5007"
 
 exports.joinChannelByUsername = async (username) => {
     const r = await fetch(`${HOSTNAME}/bot/join?username=${encodeURIComponent(username)}`, {
